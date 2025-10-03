e4e31a73174a616a4d1ca0467766e16454035af7
Manasa Pasupunuri <manasapasupunuri4@gmail.com>
2025-10-04 00:05:46 +0530

    Time: 2 ms (98.93%), Space: 45.1 MB (31.93%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] num, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < num.length; i++) {
+            int complement = target - num[i];
+            if (map.containsKey(complement)) {
+                return new int[] {map.get(complement), i};
+            }
+            map.put(num[i], i);
+        }
+        return new int[] {};
+    }
+}
+        
+
